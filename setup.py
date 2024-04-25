aa33ee931c8c01c36dbd0f88b9cab2f77acf714b
Julian <45316531+klmnjulian@users.noreply.github.com>
2024-04-25 14:53:33 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonutilslib',
+    version='1.0.0',
+    description='A collection of utility functions',
+    author='Julian K.',
+    author_email='kalmanjulian08@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
